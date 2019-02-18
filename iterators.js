ea820bdf57f64de1b6909c43363d83a0ff3ad20e
unknown <retheesh@TENT-Retheesh.tamdeenrealestate.com>
2019-02-18 19:28:34 +0300

    Iterations_1

--- iterators.js
+++ iterators.js
@@ -1,3 +1,5 @@
+import { SSL_OP_PKCS1_CHECK_1 } from "constants";
+
 /*************************************
 * logger(array)
 *
@@ -5,9 +7,8 @@
 * - Logs every element of the array
 ************************************/
 const logger = function(array) {
-  // Your code here
-};
-
+   array.forEach(ar => console.log(ar));
+}
 
 /*************************************
 * toCelsius(temperatures)
@@ -20,10 +21,9 @@ const logger = function(array) {
 * The conversion is:
 *   C = (F - 32) * (5/9)
 ************************************/
-const toCelsius = function(temperatures) {
-  // Your code here
-}
-
+const toCelsius = function(temperatures)  {
+  return temperatures.map(temp => ((temp - 32) * (5/9)) );
+  };
 
 /**************************************
 * hottestDays(temperatures, threshhold)
@@ -34,7 +34,7 @@ const toCelsius = function(temperatures) {
 *   that exceed the threshhold
 ***************************************/
 const hottestDays = function(temperatures, threshhold) {
-  // Your code here
+  return temperatures.filter(temp => temp > threshhold);
 }
 
 
@@ -53,7 +53,7 @@ const hottestDays = function(temperatures, threshhold) {
 *       all previous functions
 *******************************************/
 const logHottestDays = function(temperatures, threshhold) {
-  // Your code here
+  logger(toCelsius(hottestDays(temperatures, threshhold)))
 }
 
 
